8ec2973290ecff78da750afbd5e9af3bfe533188
mark <wngshi1978@gmail.com>
2022-10-08 22:59:39 +0800

    new file:   canister_ids.json 	modified:   dfx.json 	modified:   src/hello_backend/main.mo 	modified:   src/hello_frontend/src/index.html 	modified:   src/hello_frontend/src/index.js

--- src/hello_backend/main.mo
+++ src/hello_backend/main.mo
@@ -1,5 +1,117 @@
-actor {
+/*actor {
   public func greet(name : Text) : async Text {
     return "Hello, " # name # "!";
   };
+};
+*/
+
+import List "mo:base/List";
+import Iter "mo:base/Iter";
+import Principal "mo:base/Principal";
+import Time "mo:base/Time";
+
+actor {
+
+  public type Message = {
+    content:Text;
+    author : Text;
+    time:Time.Time;
+  };
+
+  public type Microblog = actor {
+    follow: shared (Principal) -> async();                // 添加关注对象
+    follows: shared query () -> async[Principal];         //返回关注列表
+    post: shared (Text) -> async();                       //发布新的消息
+    posts: shared query (Time.Time) ->async[Message];     //返回所有发布的消息
+    timeline: shared(Time.Time)->async [Message];         //返回所有关注对象发布的消息
+    set_name :shared (Text) ->async();
+    get_name: shared ()->async(Text);
+  // followsPrincipalId: shared query () -> async[Text]; //显示principalId 的字符串
+  //  messageCaller :shared () -> async Text;
+    
+  };
+
+  stable var followed : List.List<Principal> = List.nil();
+  stable var authorName:Text = "";
+
+  public shared func set_name (author:Text):async (){
+    authorName := author;
+  };
+
+  public shared query func get_name ():async (Text){
+    return authorName;
+  };
+
+  public shared func follow (id: Principal) : async () {
+    followed := List.push(id, followed);
+  };
+
+  public shared query func follows():async [Principal] {
+    List.toArray (followed)
+  };
+
+ stable var messages: List.List<Message> = List.nil();
+
+  public shared (msg) func post (otp:Text, text: Text) : async () {
+    //assert(Principal.toText(msg.caller) == "7w4xi-6nls6-o2gwk-p44rt-hyhqs-jlwlo-lddkp-e2wjz-azmbf-54zcj-hae");
+    assert (otp == "123456");
+    let message : Message = {
+      content = text;
+      time = Time.now();
+      author = authorName;
+      };
+     messages := List.push (message, messages)
+  };
+
+  public shared query func posts(since: Time.Time): async [Message] {  
+    var all : List.List<Message> = List.nil();
+    for (msg in Iter.fromList(messages))
+    {
+      if(msg.time>=since)
+      {
+        all :=List.push(msg,all);
+      }
+    };
+    List.toArray (all)
+  };
+
+    public shared query func posts_all(): async [Message] {  
+    var all : List.List<Message> = List.nil();
+    for (msg in Iter.fromList(messages))
+    {
+      all :=List.push(msg,all);
+    };
+    List.toArray (all)
+  };
+
+
+  public shared func timeline(since: Time.Time) : async [Message] {
+    var all : List.List<Message> = List.nil();
+    for (id in Iter.fromList(followed)) {
+        let canister : Microblog = actor(Principal.toText(id));
+        let msgs = await canister.posts(since);
+        for (msg in Iter.fromArray(msgs)) 
+        {
+            all :=List.push(msg,all);
+        }
+    };
+    List.toArray(all)
+  };
+
+
+ /* public shared query func followsPrincipalId():async [Text] {
+    
+    var all : List.List<Text> = List.nil();
+    for (id in Iter.fromList(followed)) {
+      all :=List.push(Principal.toText(id),all);
+    };
+    List.toArray(all)
+  };
+
+  public shared (msg) func messageCaller():async Text{
+    Principal.toText(msg.caller)
+  };
+*/
+
+
 };
--- src/hello_frontend/src/index.js
+++ src/hello_frontend/src/index.js
@@ -1,19 +1,153 @@
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory as hello_backend_idl, canisterId as hello_backend_id } from "../../declarations/hello_backend";
 import { hello_backend } from "../../declarations/hello_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await hello_backend.greet(name);
+async function post(){
+  let post_button = document.getElementById("post");
+  let error = document.getElementById("error");
+  error.innerText = "";
+  post_button.disabled= true;
+  let textarea = document.getElementById("message");
+  let otp = document.getElementById("otp").value;
+  let text = textarea.value;
+  let authorName = document.getElementById("author").value;
+  try{
+    await hello_backend.set_name(authorName);
+    await hello_backend.post(otp,text);
+    textarea.value = "";
+  } catch (err) {
+    console.log(err)
+    error.innerText ="Post Failed !";
+    
+  }
+  post_button.disabled = false;
+}
 
-  button.removeAttribute("disabled");
+var num_posts = 0;
+var num_follows = 0;
 
-  document.getElementById("greeting").innerText = greeting;
+async function load_posts(){
+//let posts_section = document.getElementById("posts");
 
-  return false;
-});
+//显示本地所有的信息
+let posts = await hello_backend.posts_all();
+if(num_posts == posts.length) return;
+//posts_section.replaceChildren([]);
+let tbOwn = document.getElementById("TbOwn");
+tbOwn.replaceChildren([]);
+num_posts = posts.length;
+
+for (var i=0;i<posts.length;i++){
+    //let post = document.createElement("P");
+    //post.innerText = posts[i].content;
+    //posts_section.appendChild(post)
+    var tr = document.createElement('tr');
+    tbOwn.appendChild(tr);
+
+    //显示作者
+    var td = document.createElement('td');
+    td.innerHTML = posts[i].author;
+    tr.appendChild(td);
+    
+    //显示时间
+    var dateTime = parseInt(posts[i].time);
+    var date = new Date(dateTime/1000000);
+    td = document.createElement('td');
+    var Y = date.getFullYear() + '-';
+    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    var D = date.getDate() + ' ';
+    var h = date.getHours() + ':';
+    var m = date.getMinutes() + ':';
+    var s = date.getSeconds(); 
+    td.innerHTML = Y+M+D+h+m+s;
+    tr.appendChild(td);
+
+    //显示内容
+    td = document.createElement('td');
+    td.innerHTML = posts[i].content;
+    tr.appendChild(td);
+  }
+  //显示follow的作者名字
+  let follows_section = document.getElementById("follows");
+  let follows = await hello_backend.follows();
+  if(num_follows == follows.length) return;
+  follows_section.replaceChildren([]);
+  num_follows = follows.length;
+
+  let agent = new HttpAgent();
+  for (var i=0;i<follows.length;i++){
+    let follow_section_Child = document.createElement("P");
+    let Microblog = Actor.createActor(hello_backend_idl,{agent:agent, canisterId:follows[i]});
+    let followName = await Microblog.get_name();
+    //follow_section_Child.innerHTML = "<a href='javascript:void(0)' onclick = DisplayFollow("+ follows[i].toText()+ ")>"+followName+"</ a>";
+    follow_section_Child.innerHTML = "<a href='javascript:void(0)'>"+followName+"</ a>";
+   // follow_section_Child.addEventListener("click",DisplayFollow,false);
+   follow_section_Child.addEventListener("click",DisplayFollow,false);
+    follow_section_Child.setAttribute('followIDName',follows[i].toText())
+    follows_section.appendChild(follow_section_Child)
+  }
+}
+
+function DisplayFollow1(){
+  console.log('test', 11111);
+}
+
+
+async function DisplayFollow(event){
+
+  let followIDName = event.currentTarget.attributes.followIDName.value;
+ // console.log(event.currentTarget.attributes.followIDName.value);
+  let follows = await hello_backend.follows(); 
+  let agent = new HttpAgent();
+  for (var i=0;i<follows.length;i++){
+    let followId = follows[i].toText();
+    if(followId == followIDName)  //表明ID相同
+    {
+      let tbfollows = document.getElementById("TbFollows");
+      tbfollows.replaceChildren([]);
+      let Microblog = Actor.createActor(hello_backend_idl,{agent:agent, canisterId:follows[i]});
+      let posts = await Microblog.posts_all();
+      for (var i=0;i<posts.length;i++){
+
+        var tr = document.createElement('tr');
+        tbfollows.appendChild(tr);
+    
+        //显示作者
+        var td = document.createElement('td');
+        td.innerHTML = posts[i].author;
+        tr.appendChild(td);
+        
+        //显示时间
+        var dateTime = parseInt(posts[i].time);
+        var date = new Date(dateTime/1000000);
+        td = document.createElement('td');
+        var Y = date.getFullYear() + '-';
+        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+        var D = date.getDate() + ' ';
+        var h = date.getHours() + ':';
+        var m = date.getMinutes() + ':';
+        var s = date.getSeconds(); 
+        td.innerHTML = Y+M+D+h+m+s;
+        tr.appendChild(td);
+    
+        //显示内容
+        td = document.createElement('td');
+        td.innerHTML = posts[i].content;
+        tr.appendChild(td);
+      }
+      return;
+    }
+    
+  }
+}
+
+function load(){
+  let post_button = document.getElementById("post");
+  post_button.onclick = post;
+  load_posts()
+  setInterval(load_posts,3000);
+}
+window.onload = load
